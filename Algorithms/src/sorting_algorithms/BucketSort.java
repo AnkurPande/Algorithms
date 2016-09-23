74edc9a448e1aeeca9f9db349c1a82c02c2dedea
AnkurPande <ankur.pandey111@gmail.com>
2016-09-22 20:13:28 -0400

    Bucket and Counting

---
+++ Algorithms/src/sorting_algorithms/BucketSort.java
@@ -0,0 +1,14 @@
+package sorting_algorithms;
+
+class BucketSort {
+
+	public BucketSort() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Algorithms/src/sorting_algorithms/CountingSort.java
@@ -0,0 +1,18 @@
+package sorting_algorithms;
+
+
+public class CountingSort {
+
+	int[] array = null;
+	int maxVal = 0;
+	
+	public void countingSort(){
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
